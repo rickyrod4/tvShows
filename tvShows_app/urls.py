b32a765be8e9f4ff643f04d89b82a6bed2bc072c
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-07-19 12:40:24 -0500

    Updated with 3 more pages as well as delete and show tags

--- tvShows_app/urls.py
+++ tvShows_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('shows', views.allShows),
+    path('shows/new',views.new),
+    path('create', views.create),
+    path('shows/<int:show_id>', views.show),
+    path('delete/<int:show_id>', views.delete)
 ]
--- tvShows_app/views.py
+++ tvShows_app/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
+from .models import Show
 
 # Create your views here.
-def index(request):
-    return HttpResponse('Hello World')
+def allShows(request):
+    context = {
+        'shows' : Show.objects.all()
+    }
+    return render(request,'allShows.html', context)
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    Show.objects.create(
+        title = request.POST['title'], 
+        network = request.POST['network'],
+        releaseDate = request.POST['releaseDate'],
+        description = request.POST['description']
+    )
+    return redirect('/shows')
+
+def show(request, show_id):
+    context = {
+        'show' : Show.objects.get(id=show_id)
+    }
+    return render(request,'showInfo.html',context)
+
+def delete(request, show_id):
+    Show.objects.get(id=show_id).delete()
+    return redirect('/shows')
